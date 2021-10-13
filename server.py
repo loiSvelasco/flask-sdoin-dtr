ec8bf1949dfa3740c3b6e6917dd660be31d0c1c4
Louis Velasco <loisuperficialvelasco@gmail.com>
2021-10-13 16:24:52 +0800

    Added waitress

--- server.py
+++ server.py
@@ -1,3 +1,3 @@
 from waitress import serve
 import app
-serve(app.app, host='192.168.1.4', port=80)
+serve(app.app, host='192.168.1.105', port=5000, threads=8)
---
+++ waitress-pylons-server.bat
@@ -0,0 +1,10 @@
+@ECHO off
+color 0b
+title Server running ~sdoin.dtr @ ipv4:5000 ~ by Louis Velasco 
+ECHO Serving ~sdoin.dtr on : http://192.168.1.105:5000
+ECHO Do not close this window.
+ping 192.168.1.105
+call venv-dtr\Scripts\activate
+python server.py
+pause
+
