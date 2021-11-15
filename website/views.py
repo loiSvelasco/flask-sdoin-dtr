1a1871a7683f1b14dc5a6deee2096826e1bbfd8f
Louis Velasco <loisuperficialvelasco@gmail.com>
2021-11-15 09:41:32 +0800

    Disable logging for logged off users

--- website/views.py
+++ website/views.py
@@ -11,66 +11,72 @@ views = Blueprint('views', __name__)
 
 @views.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        id_no = request.form.get('idno')
-        staff = Staff.query.filter_by(staff_id_no=id_no).first()
+        if request.method == 'POST':
 
-        if not staff:
-            flash('No staff with that ID.', category='error')
-            return render_template('index.html', user=current_user)
+            if current_user.is_authenticated: # To prevent employees from timing in/out from their personal computers.
+                id_no = request.form.get('idno')
+                staff = Staff.query.filter_by(staff_id_no=id_no).first()
+            else: # Needs admin account to register time in/out 
+                flash('Logging for your device is prohibited. Please use the proper terminal to register your in and out of work hours.', category='error')
+                return render_template('index.html', user=current_user)
+            
 
-        dt = datetime.now()
-        today_dt = date.today()
+            if not staff:
+                flash('No staff with that ID.', category='error')
+                return render_template('index.html', user=current_user)
 
-        dtr_log = DailyTimeRecord.query.filter(
-            func.date(DailyTimeRecord.time_in_am)==today_dt, 
-            DailyTimeRecord.staff_id==staff.id
-        ).first()
+            dt = datetime.now()
+            today_dt = date.today()
 
-        if dtr_log:
-            if dtr_log.time_out_am == None:
-                a, b = dtr_log.time_in_am, dt
-                c = b-a
-                mins = c.seconds / 60
-                if mins < 1:
-                    flash('Hello ' + staff.name.split(' ', 1)[0] + '! You have just timed-in for the morning, to avoid duplicate records, try again in a minute.', category='error')
-                    return render_template('index.html', user=current_user)
-                else:
-                    dtr_log.time_out_am = dt
-                    db.session.commit()
-                    return render_template('notice.html', user=current_user, staff=staff, time=dt.strftime('%I:%M %p'), day=dt.strftime('%A, %B %d'), status='TIME OUT - AM', message='Enjoy your lunch ' + staff.name.split(' ',1)[0] + '!')
-            elif dtr_log.time_in_pm == None:
-                a, b = dtr_log.time_out_am, dt
-                c = b-a
-                mins = c.seconds / 60
-                if mins < 1:
-                    flash('Hello ' + staff.name.split(' ', 1)[0] + '! You have just timed-out for the morning, to avoid duplicate records, try again in a minute.', category='error')
-                    return render_template('index.html', user=current_user)
-                else:
-                    dtr_log.time_in_pm = dt
-                    db.session.commit()
-                    return render_template('notice.html', user=current_user, staff=staff, time=dt.strftime('%I:%M %p'), day=dt.strftime('%A, %B %d'), status='TIME IN - PM', message='Welcome back ' + staff.name.split(' ',1)[0] + '! How was your lunch?')
-            elif dtr_log.time_out_pm == None:
-                a, b = dtr_log.time_in_pm, dt
-                c = b-a
-                mins = c.seconds / 60
-                if mins < 1:
-                    flash('Hello ' + staff.name.split(' ', 1)[0] + '! You have just timed-in for the afternoon, to avoid duplicate records, try again in a minute.', category='error')
-                    return render_template('index.html', user=current_user)
+            dtr_log = DailyTimeRecord.query.filter(
+                func.date(DailyTimeRecord.time_in_am)==today_dt, 
+                DailyTimeRecord.staff_id==staff.id
+            ).first()
+
+            if dtr_log:
+                if dtr_log.time_out_am == None:
+                    a, b = dtr_log.time_in_am, dt
+                    c = b-a
+                    mins = c.seconds / 60
+                    if mins < 1:
+                        flash('Hello ' + staff.name.split(' ', 1)[0] + '! You have just timed-in for the morning, to avoid duplicate records, try again in a minute.', category='error')
+                        return render_template('index.html', user=current_user)
+                    else:
+                        dtr_log.time_out_am = dt
+                        db.session.commit()
+                        return render_template('notice.html', user=current_user, staff=staff, time=dt.strftime('%I:%M %p'), day=dt.strftime('%A, %B %d'), status='TIME OUT - AM', message='Enjoy your lunch ' + staff.name.split(' ',1)[0] + '!')
+                elif dtr_log.time_in_pm == None:
+                    a, b = dtr_log.time_out_am, dt
+                    c = b-a
+                    mins = c.seconds / 60
+                    if mins < 1:
+                        flash('Hello ' + staff.name.split(' ', 1)[0] + '! You have just timed-out for the morning, to avoid duplicate records, try again in a minute.', category='error')
+                        return render_template('index.html', user=current_user)
+                    else:
+                        dtr_log.time_in_pm = dt
+                        db.session.commit()
+                        return render_template('notice.html', user=current_user, staff=staff, time=dt.strftime('%I:%M %p'), day=dt.strftime('%A, %B %d'), status='TIME IN - PM', message='Welcome back ' + staff.name.split(' ',1)[0] + '! How was your lunch?')
+                elif dtr_log.time_out_pm == None:
+                    a, b = dtr_log.time_in_pm, dt
+                    c = b-a
+                    mins = c.seconds / 60
+                    if mins < 1:
+                        flash('Hello ' + staff.name.split(' ', 1)[0] + '! You have just timed-in for the afternoon, to avoid duplicate records, try again in a minute.', category='error')
+                        return render_template('index.html', user=current_user)
+                    else:
+                        dtr_log.time_out_pm = dt
+                        db.session.commit()
+                        return render_template('notice.html', user=current_user, staff=staff, time=dt.strftime('%I:%M %p'), day=dt.strftime('%A, %B %d'), status='TIME OUT - PM', message='Good work today ' + staff.name.split(' ',1)[0] + '!')
                 else:
                     dtr_log.time_out_pm = dt
                     db.session.commit()
                     return render_template('notice.html', user=current_user, staff=staff, time=dt.strftime('%I:%M %p'), day=dt.strftime('%A, %B %d'), status='TIME OUT - PM', message='Good work today ' + staff.name.split(' ',1)[0] + '!')
             else:
-                dtr_log.time_out_pm = dt
+                new_dtr_log = DailyTimeRecord(time_in_am=dt, time_out_am=None, time_in_pm=None, time_out_pm=None, staff_id=staff.id)
+                db.session.add(new_dtr_log)
                 db.session.commit()
-                return render_template('notice.html', user=current_user, staff=staff, time=dt.strftime('%I:%M %p'), day=dt.strftime('%A, %B %d'), status='TIME OUT - PM', message='Good work today ' + staff.name.split(' ',1)[0] + '!')
-        else:
-            new_dtr_log = DailyTimeRecord(time_in_am=dt, time_out_am=None, time_in_pm=None, time_out_pm=None, staff_id=staff.id)
-            db.session.add(new_dtr_log)
-            db.session.commit()
-            return render_template('notice.html', user=current_user, staff=staff, time=dt.strftime('%I:%M %p'), day=dt.strftime('%A, %B %d'), status='TIME IN - AM', message='Enjoy your day ' + staff.name.split(' ',1)[0] + '!')
-    return render_template('index.html', user=current_user)
+                return render_template('notice.html', user=current_user, staff=staff, time=dt.strftime('%I:%M %p'), day=dt.strftime('%A, %B %d'), status='TIME IN - AM', message='Enjoy your day ' + staff.name.split(' ',1)[0] + '!')
+        return render_template('index.html', user=current_user)
 
 
 @views.route('/logs')
