470e6863ca964228fa400a99d4d94fcb6db1299d
Louis Velasco <21021463+loiSvelasco@users.noreply.github.com>
2022-05-03 12:26:57 +0800

    Add back page of CSC Form 48

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from website import create_app
 from flask import render_template
 from flask_login import current_user
-
+from waitress import serve
 
 app = create_app()
 
@@ -10,7 +10,8 @@ def not_found(e):
     return render_template('404.html', user=current_user), 404
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    serve(app.app, host='192.168.1.4', port=5000, threads=8)
 
 #                           CHANGE TO UR IPV4
 # * To run: waitress-serve --host=192.168.101.193 --port=80 --call "app:create_app"
